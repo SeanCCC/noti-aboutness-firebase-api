fed466aa052c813715b120bc12bbe6f1c10cdd5f
seanccc <sean831004@gmail.com>
2020-02-16 15:51:32 +0800

    routing config

---
+++ site/src/pages/Recruit/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from './Form';
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
+
+export default function Recruit() {
+    let match = useRouteMatch();
+  
+    return (
+      <div>
+        <Switch>
+          <Route path={`${match.path}/form`}>
+            <Form/>
+          </Route>
+          <Route path={match.path}>
+            <h3>Please select a topic.</h3>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
