9893e835f199becefe2c2b60028cbf66b6d9667e
seanccc <sean831004@gmail.com>
2020-02-16 16:56:47 +0800

    eslint setup

--- site/src/pages/Recruit/index.js
+++ site/src/pages/Recruit/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Form from './Form';
+import React from 'react'
+import Form from './Form'
 import {
   Switch,
   Route,
-  useRouteMatch,
-} from "react-router-dom";
+  useRouteMatch
+} from 'react-router-dom'
 
-export default function Recruit() {
-    let match = useRouteMatch();
-  
-    return (
-      <div>
-        <Switch>
-          <Route path={`${match.path}/form`}>
-            <Form/>
-          </Route>
-          <Route path={match.path}>
-            <h3>Please select a topic.</h3>
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
+export default function Recruit () {
+  const match = useRouteMatch()
+
+  return (
+    <div>
+      <Switch>
+        <Route path={`${match.path}/form`}>
+          <Form/>
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
