abc52e2cd047823ef08799c609a0140dcd4da21f
seanccc <sean831004@gmail.com>
2020-03-10 17:54:12 +0800

    sample site of panel

--- panel/src/pages/Recruit/index.js
+++ panel/src/pages/Recruit/index.js
@@ -1,44 +1,20 @@
 import React from 'react'
-import Introduction from './Introduction'
-import {
-  Switch,
-  Route,
-  useRouteMatch,
-  Redirect
-} from 'react-router-dom'
-import FormPage from './FormPage'
-import MailCheckPage from './MailCheck'
-import { AcceptPage, ReturnPage, EmailCheckPage, ErrorPage, RepeatPage } from './ResultPage'
+// import {
+//   Switch,
+//   Route,
+//   useRouteMatch,
+//   Redirect
+// } from 'react-router-dom'
 
 export default function Recruit () {
-  const match = useRouteMatch()
-  return (
-    <Switch>
-      <Route path={`${match.path}/introduction`}>
-        <Introduction/>
-      </Route>
-      <Route path={`${match.path}/form`}>
-        <FormPage />
-      </Route>
-      <Route path={`${match.path}/accept`}>
-        <AcceptPage />
-      </Route>
-      <Route path={`${match.path}/mail`}>
-        <EmailCheckPage />
-      </Route>
-      <Route path={`${match.path}/mailcheck`} component={MailCheckPage}/>
-      <Route path={`${match.path}/return`}>
-        <ReturnPage />
-      </Route>
-      <Route path={`${match.path}/repeat`}>
-        <RepeatPage />
-      </Route>
-      <Route path={`${match.path}/error`}>
-        <ErrorPage />
-      </Route>
-      <Route path={match.path}>
-        <Redirect to={`${match.path}/introduction`} />
-      </Route>
-    </Switch>
-  )
+  // const match = useRouteMatch()
+  // return (
+  //   <Switch>
+  //     <Route path={`${match.path}/orientation`} component={checkId(Orientation)}/>
+  //     <Route path={match.path}>
+  //       <Redirect to={`${match.path}/orientation`} />
+  //     </Route>
+  //   </Switch>
+  // )
+  return <div>recruit</div>
 }
