8586a362c4c4113d18bfacaca7c8283a13570992
SeanCCC <sean831004@gmail.com>
2020-11-17 19:02:42 +0800

    修正領據流程

--- site/src/pages/Participant/Compensation/MailMethod.js
+++ site/src/pages/Participant/Compensation/MailMethod.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Header, Segment, Checkbox, Button, Message } from 'semantic-ui-react'
+import { Header, Segment, Checkbox, Button, Message, Image } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
+import LabMap from '../LabMap'
 import { ContactComp } from '../../Contact'
-import MailInstruction from '../MailInstruction'
 
 export default class MailMethod extends Component {
   constructor (props) {
@@ -42,9 +42,35 @@ export default class MailMethod extends Component {
         3.如果選擇郵寄，請盡可能以掛號方式寄出，這可以確保信件一定會到達，以避免您不必要的困擾。<br/>
         4.選擇郵寄時，如果因故無法使用掛號，請使用限時郵件。<br/>
         5.所有影印、郵務方面支出，均已經包含在報酬中。<br/>
-        6.請在選取交件方式點選『選擇收款途徑』（在下方）<br/>
+        6.請選取交件方式並點選『選擇收款途徑』（在下方）<br/>
         </Segment>
-        <MailInstruction/>
+        <Segment attached>
+          <Header as='h3'
+            textAlign="center">文書寄出資訊</Header>
+        您需要將同意書郵寄至實驗室，盡量採用掛號的方法。<br/>
+        實測4張紙與一個信封不超過50克，限時郵件郵票可貼23元。<br/>
+        收件人：張忠喬 先生<br/>
+        聯絡電話：0975-068-858<br/>
+        地址：30010新竹市東區大學路1001號交通大學電子與資訊研究中心715室<br/>
+        </Segment>
+        <Segment attached>
+          <Header as='h3'
+            textAlign="center">同意書親自交付資訊</Header>
+        請直接將同意書投入郵箱即可<br/>
+        郵箱位址：新竹市東區大學路1001號交通大學電子與資訊研究中心二樓33號信箱<br/>
+        門禁時間：防疫期間下午四點後需要刷卡進出，非防疫期間晚上七點後需要刷卡進出
+          <Header as='h4'
+            textAlign="center">實驗室地圖</Header>
+          <LabMap/>
+          <Header as='h4'
+            textAlign="center">郵箱位置圖</Header>
+          <Image fluid
+            src="https://storage.googleapis.com/noti-aboutness-firebase-48728.appspot.com/2FMap.jpg"/>
+          <Image fluid
+            src="https://storage.googleapis.com/noti-aboutness-firebase-48728.appspot.com/boxes.jpg"/>
+          <Image fluid
+            src="https://storage.googleapis.com/noti-aboutness-firebase-48728.appspot.com/box.jpg"/>
+        </Segment>
         <Segment attached>
           <Header as='h3'
             textAlign="center">交件方式選擇</Header>
