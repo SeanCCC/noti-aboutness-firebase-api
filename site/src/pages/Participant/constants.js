85ad7d245a04ef95871e199654379223e1be595c
SeanCCC <sean831004@gmail.com>
2020-12-17 20:37:50 +0800

    change apk link

--- site/src/pages/Participant/constants.js
+++ site/src/pages/Participant/constants.js
@@ -1,4 +1,4 @@
-export const apkFileLink = 'https://firebasestorage.googleapis.com/v0/b/noti-aboutness-firebase-48728.appspot.com/o/app-debug-2.0.8-B.apk?alt=media&token=10744971-2d53-4d0f-a9b0-230ad95dd365'
+export const apkFileLink = 'https://storage.googleapis.com/noti-aboutness-firebase-48728.appspot.com/app-debug-2.2.1.apk'
 export const consentFileLink = 'https://storage.googleapis.com/noti-aboutness-firebase-48728.appspot.com/%E8%AE%8A%E6%9B%B4%E6%A0%B8%E5%AE%9A%E7%89%88_%E5%8F%83%E8%88%87%E8%80%85%E5%90%8C%E6%84%8F%E6%9B%B8V4.pdf'
 export const batteryLinkTable = {
   asus: 'https://docs.google.com/document/d/1Bg6TyPAzUXZy9XaIZiNzPyQGroa4nF2Cx3j36Vwix34/edit#heading=h.hw5o2t1m5uc0',
