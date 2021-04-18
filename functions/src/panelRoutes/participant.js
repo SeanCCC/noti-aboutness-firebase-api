da7857d6a6c38c90f20b743d15b2e6c0cffa3459
SeanCCC <sean831004@gmail.com>
2021-04-18 08:55:19 +0800

    minor fix

--- functions/src/panelRoutes/participant.js
+++ functions/src/panelRoutes/participant.js
@@ -149,14 +149,22 @@ router.post('/payment/ask', async (req, res) => {
     const p = await fetchDB(`participant/${uid}`)
     // 遠端支付的部份
     const compensation = interview ? (p.compensation + 300) : p.compensation
-    const _interviewStatus = p.status === status.INTERVIEWEE ? interviewStatus.DONE : p.interviewStatus
-    await updateDB(`participant/${uid}`, {
-      compensation,
-      status: status.SET_RECEIPT_MAIL_METHOD,
-      askPaymentTime: now,
-      lastStatusChanged: now,
-      interviewStatus: _interviewStatus
-    })
+    if (p.status === status.INTERVIEWEE) {
+      await updateDB(`participant/${uid}`, {
+        compensation,
+        status: status.SET_RECEIPT_MAIL_METHOD,
+        askPaymentTime: now,
+        lastStatusChanged: now,
+        interviewStatus: interviewStatus.DONE
+      })
+    } else {
+      await updateDB(`participant/${uid}`, {
+        compensation,
+        status: status.SET_RECEIPT_MAIL_METHOD,
+        askPaymentTime: now,
+        lastStatusChanged: now
+      })
+    }
     await askPaymentMail(uid)
     res.send('success')
   } catch (err) {
