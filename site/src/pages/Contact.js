7a7a961f83c427ace7070b51e671dafe8ebf4362
SeanCCC <sean831004@gmail.com>
2021-05-24 11:27:06 +0800

    change title

--- site/src/pages/Contact.js
+++ site/src/pages/Contact.js
@@ -13,7 +13,7 @@ notiatmuilab@gmail.com<br/>
       </Segment>
       <Segment attached>
     研究計畫主持人<br/>
-張永儒, 助理教授<br/>
+張永儒, 副教授<br/>
 國立交通大學資訊工程學系<br/>
 (03) 5712121 #56632<br/>
 armuro@cs.nctu.edu.tw<br/>
