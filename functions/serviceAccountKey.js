a4837086ae7bfa70eac648ba970b984dc5143db8
Sean Chang <sean831004@gmail.com>
2025-02-06 10:16:24 +0800

    Update serviceAccountKey.js

--- functions/serviceAccountKey.js
+++ functions/serviceAccountKey.js
@@ -1,8 +1,8 @@
 module.exports = {
   type: 'service_account',
   project_id: 'noti-aboutness-firebase-48728',
-  private_key_id: 'ca3c187218eec8cf73901d5fb4c3f50a42a3f070',
-  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDA8Cu3D7m0aHWO\nAwbwWjwaaGIHWXYR9vsEekdYe+sApBkMRHVofn9raThRKjtEmXptrOpVKjAv1x6h\nHnRkxu+WEGgxoAqtJ37kHLNhnD97hnaN7/YsqoA1a67s+1YY/JO3cwE2KMD+GYV2\n3FCPbkO8ORmrI7s7/5MR+Q0M9aSP3c/5aHtEY3JrgH5c48/akOLJjSSl1Ap/TrRq\n7Rxvu7/nCj5HQiF4Rlx1qAwXBTtXqo/sXw5ZxZj4iJSZpgeCPZ4MezRubV1ZZuad\nBB2IM4OO+vO6jJu/mfks+t6v+SgvrPP538Ir6NGZ4CgMtTN3nnY8MT8mFTcUUGVu\n5FDhmrbZAgMBAAECggEAD3cI1//0j5WKa69KLnibJfY9Ok/VTxgUE44uGxV2tyrQ\nH6u6mf0+vyg0VYwsK0Henn0sXNZ4fZMxjzJa28uW33lR8r1ccbfvW79BsSamL4Nu\nDA9ClR63D2Eh6Li2NGNFf2BCZfoTCyMWT7pV4kNRPlx41l8TNlpUam3DOilT7xyR\nmbdNfxBUPVzfUapZ6I39I0Hjsvtg0hodE+MidnjzENKeSh20e0IgQm6PNrBUxAcd\nFdTGtAbqvPAwfVdzdEbkebU2CaAb7emf1hQTsD3Vne+XBVKutw02mHswR00jgcmZ\nsuhJ9KagG+Srv6+LDKataRZWE5AQ7BgYx7lEo8zacQKBgQDx/9B/568vfwaeJzpe\nCmr/ip3o0goWKIyG3GFJad3gnhfWCNIJfRPTGwvYGygfOYKBgjs2enuvb9AJHhtA\nnH1kVcqtSLL/JliqmtgBGd43kJq04G8JkH1mEuNumUDLhUsbGeTnL306BssCEomY\nmDbDvKvufklby3l2KwiEhS/JKQKBgQDMGbm+UYjYCXwmvIi5Ej2sEXWUiiTEAL+G\ngzlFoaIsLBT720qCgD7C9BKim4wLfQ0oBTVws7vitNJT/Ip9CBpVG9smsdWCb/gX\naZHJC7DLLe9uVARcscF94zXmyldqzsrqfELEeW86ce/LM29/KJdAYI0ZJ1LHyibC\nV5Mk6VRGMQKBgQCBsZrZYZ6GsuomCloCmky7xVtZsm+LbpHZmPRetukdfzzBDZsb\nULg2NFGFsRAwo86of9YiHUR6Qk9ZA5SRbQzaoAvuEH81tSRh0nifJnDskIgJFhsM\nIi7CbK8Mz/9k03vr+1Ws/Mt9NLTJ2It7h2AaSNxQBchk9MiUpv16oDJ6+QKBgQC4\nS/qHOkShH1918wRaaXg66cEMkflE65AGPHP4xk0UKfIAwjFzya3iiFZTcrVwokjA\n/IMW85USz0ilAgNjh2+PromjVS2AjO6u5Wr46Y+IyD/NlhwiP3iC3ioL7mYlUfm0\n5uO7WhPv6HYpPJbPca2yOfzu261KLuX00vNRf0fT4QKBgGJFvjoY7vbJ+UIPyI27\nUtYuZMngSFS7vRVki6qjKZuqJ6/QaovOeTpXCYrZ1+NAn34H1MOWZU2XrWg23ocb\nQKm8/7n9DZHte59D5jxCwtnvX/LRlduOHXm/cxzcoPLuwwutjmoOAuUC8dEsgdwq\n4Ojoo2WYQ+t3PIlDFsbTmBnm\n-----END PRIVATE KEY-----\n',
+  private_key_id: 'xxxx',
+  private_key: 'xxxx',
   client_email: 'firebase-adminsdk-8aqm2@noti-aboutness-firebase-48728.iam.gserviceaccount.com',
   client_id: '101190176546615272359',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
